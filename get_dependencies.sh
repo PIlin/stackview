279d408bea932d35678abb9ed462aa9439a83f9b
Pavel Ilin <ilin.pa@gmail.com>
2013-02-22 13:42:36 +0100

    patch for ELFIO-1.0.3, fixing crash on mac os x x64

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -7,5 +7,7 @@ curl --location -O http://downloads.sourceforge.net/project/elfio/ELFIO-sources/
 tar -xzf ELFIO-1.0.3.tar.gz
 
 cd ELFIO-1.0.3
+patch -p1 -i ../ELFIO_stdint.patch
 ./configure --prefix=`pwd`/install_root
 make && make install
+cd ..
